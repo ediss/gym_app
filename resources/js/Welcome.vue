98d89486e47f91d42a2427b45393933a1fc21a6a
ediss <skenderi.e94@gmail.com>
2023-04-11 21:22:16 +0200

    My first Vue Component

---
+++ resources/js/Welcome.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-1">
+            <div class="col-md-8 mt-5">
+                <div class="text-center">
+                    <h1 class="display-4">{{ greeting }}</h1>
+                    <p class="lead">{{ message }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            greeting: 'Welcome to Vue.js with Bootstrap!',
+            message: 'Thank you for using ChatGPT.',
+        };
+    },
+};
+</script>
