6bc777866748d38e7ff7b53efeab210ac0a2a8ef
ediss <skenderi.e94@gmail.com>
2024-04-15 01:47:03 +0200

    small cosmetics

--- resources/views/web/coach/appointments/appointments.blade.php
+++ resources/views/web/coach/appointments/appointments.blade.php
@@ -47,8 +47,8 @@
         <div class="col-12">
 
             <ul class="nav nav-tabs nav-warning d-flex border-success" role="tablist">
-                <li class="nav-item px-0" role="presentation">
-                    <a class="nav-link {{ $startedAppointments === 0 ? 'active' : '' }}" data-bs-toggle="tab" href="#appointments-pending" role="tab"
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link px-0 {{ $startedAppointments === 0 ? 'active' : '' }}" data-bs-toggle="tab" href="#appointments-pending" role="tab"
                        aria-selected="true">
                         <div class="d-flex align-items-center">
                             <div class="tab-icon">
