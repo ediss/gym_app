b551f6bed64854a7b1f5148d5c90afb5fa47d30a
ediss <skenderi.e94@gmail.com>
2024-04-17 21:34:19 +0200

    force install

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -38,18 +38,6 @@
     <script src="{{ asset('frontend/js/main.js') }}"></script>
 
     @yield('scripts')
-
-    <script type="text/javascript">
-        if ('serviceWorker' in navigator) {
-            console.log("Will the service worker register?");
-            navigator.serviceWorker.register('/public/frontend/js/service-worker.js')
-                .then(function(reg) {
-                    console.log("Yes, it did.");
-                }).catch(function(err) {
-                    console.log("No it didn't. This happened:", err)
-                });
-        }
-    </script>
 </body>
 
 </html>
