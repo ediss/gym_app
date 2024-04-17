6611ac75dc8b11722703d72be0709f7d76815de8
ediss <skenderi.e94@gmail.com>
2024-04-17 22:17:16 +0200

    force install

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -48,17 +48,45 @@
         window.addEventListener("beforeinstallprompt", (event) => {
             event.preventDefault();
             installPrompt = event;
-
-            const result = await installPrompt.prompt();
-            console.log(`Install prompt was: ${result.outcome}`);
+            installButton.removeAttribute("hidden");
         });
 
-        addEventListener("load", (event) => {
-            const result = await installPrompt.prompt();
-            console.log(`Install prompt was: ${result.outcome}`);
-        //     disableInAppInstallPrompt();
+        window.addEventListener('DOMContentLoaded', async () => {
+            if (installPrompt) {
+                const result = await installPrompt.prompt();
+                console.log(`Install prompt was: ${result.outcome}`);
+                disableInAppInstallPrompt();
+            }
         });
 
+        function disableInAppInstallPrompt() {
+            installPrompt = null;
+            installButton.setAttribute("hidden", "");
+        }
+
+        window.addEventListener("appinstalled", () => {
+            disableInAppInstallPrompt();
+        });
+    </script>
+    {{-- <script>
+        let installPrompt = null;
+        const installButton = document.querySelector("#install");
+
+        window.addEventListener("beforeinstallprompt", (event) => {
+            event.preventDefault();
+            installPrompt = event;
+            installButton.removeAttribute("hidden");
+        });
+
+
+        installButton.addEventListener("click", async () => {
+            if (!installPrompt) {
+                return;
+            }
+            const result = await installPrompt.prompt();
+            console.log(`Install prompt was: ${result.outcome}`);
+            disableInAppInstallPrompt();
+        });
 
         function disableInAppInstallPrompt() {
             installPrompt = null;
@@ -74,7 +102,7 @@
             installPrompt = null;
             installButton.setAttribute("hidden", "");
         }
-    </script>
+    </script> --}}
 
     <script>
         function isRunningStandalone() {
