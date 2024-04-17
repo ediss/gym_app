b73d4a0eb5070de4597a4aceb0a3a7930904ee4f
ediss <skenderi.e94@gmail.com>
2024-04-17 21:21:46 +0200

    force install

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -1,44 +1,55 @@
-
 <!doctype html>
 <html lang="en" data-bs-theme="dark">
 @include('web.layouts.head')
+
 <body>
 
-{{--<!--start header-->--}}
-@include('web.layouts.header')
-<!--end header-->
+    {{-- <!--start header--> --}}
+    @include('web.layouts.header')
+    <!--end header-->
 
-<!--start sidebar-->
-@include('web.layouts.sidebar')
-<!--end sidebar-->
+    <!--start sidebar-->
+    @include('web.layouts.sidebar')
+    <!--end sidebar-->
 
-@yield('additionalHeader')
+    @yield('additionalHeader')
 
-<!--start main content-->
-<main class="page-content">
+    <!--start main content-->
+    <main class="page-content">
         @yield('content')
-</main>
-<!--end main content-->
+    </main>
+    <!--end main content-->
 
-<!--start theme customization-->
+    <!--start theme customization-->
 
-<!--end theme customization-->
-
-
-<!--plugins-->
-<script src="{{  asset('frontend/js/jquery.min.js') }}"></script>
-<script src="{{  asset('frontend/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
-<script src="{{  asset('frontend/plugins/metismenu/js/metisMenu.min.js')}}"></script>
-<script src="{{  asset('frontend/plugins/simplebar/js/simplebar.min.js')}}"></script>
-<script src="{{  asset('frontend/js/index.js') }}"></script>
-<script src="{{  asset('frontend/js/dateFormat.js') }}"></script>
-
-<!--BS Scripts-->
-<script src="{{asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{asset('frontend/js/main.js') }}"></script>
-
-@yield('scripts')
+    <!--end theme customization-->
 
 
+    <!--plugins-->
+    <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('frontend/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/index.js') }}"></script>
+    <script src="{{ asset('frontend/js/dateFormat.js') }}"></script>
+
+    <!--BS Scripts-->
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/main.js') }}"></script>
+
+    @yield('scripts')
+
+    <script type="text/javascript">
+        if ('serviceWorker' in navigator) {
+            console.log("Will the service worker register?");
+            navigator.serviceWorker.register('../../../../public/frontend/js/service-worker.js')
+                .then(function(reg) {
+                    console.log("Yes, it did.");
+                }).catch(function(err) {
+                    console.log("No it didn't. This happened:", err)
+                });
+        }
+    </script>
 </body>
+
 </html>
