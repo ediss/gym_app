26b74477ae5a396c0c6d0e1b231759eb2e1c2dc2
ediss <skenderi.e94@gmail.com>
2024-04-18 00:19:48 +0200

    trying to add functionality add to home screen for iOS

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -91,7 +91,7 @@
 
 
     <script>
-        // Detects if device is on iOS 
+        // Detects if device is iOS 
         const isIos = () => {
             const userAgent = window.navigator.userAgent.toLowerCase();
             return /iphone|ipad|ipod/.test(userAgent);
