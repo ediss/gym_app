1131db3aae730e89567784b75737739e6f757650
ediss <skenderi.e94@gmail.com>
2024-04-17 23:31:02 +0200

    force install

--- resources/views/web/coach/appointments/start-appointment.blade.php
+++ resources/views/web/coach/appointments/start-appointment.blade.php
@@ -4,7 +4,8 @@
         .accordion-button:not(.collapsed) {
             color: black;
             /*background-color: transparent;*/
-            background-color:  rgba(var(--bs-warning-rgb), var(--bs-text-opacity))!important;;
+            background-color: rgba(var(--bs-warning-rgb), var(--bs-text-opacity)) !important;
+            ;
             box-shadow: none;
 
         }
@@ -20,9 +21,8 @@
         <div class="col-12 mb-4">
             <label class="w-100">
                 <input name="search_exercises" id="searchExercises" type="text" class="form-control" value=''
-                       placeholder="Search..."
-                       oninput="searchExercises()" data-usage-type="start_appointment"
-                       data-appointment={{$appointmentID}}>
+                    placeholder="Search..." oninput="searchExercises()" data-usage-type="start_appointment"
+                    data-appointment={{ $appointmentID }}>
             </label>
         </div>
 
@@ -31,12 +31,10 @@
             <div class="card bg-transparent">
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
-                        @foreach($categories as $category)
+                        @foreach ($categories as $category)
                             <li class="d-flex justify-content-between align-items-center list-group-item category bg-transparent"
-                                data-category-id="{{$category->id}}"
-                                data-usage-type="start_appointment"
-                                data-appointment-id={{$appointmentID}}
-                            >
+                                data-category-id="{{ $category->id }}" data-usage-type="start_appointment"
+                                data-appointment-id={{ $appointmentID }}>
                                 {{ $category->name }}
                                 <span class="badge border border-warning rounded-pill font-15 ">
                                     {{ $category->exercises_count }}
@@ -60,19 +58,24 @@
 @endsection
 
 @section('scripts')
-    <script src="{{asset('exercises/exercises.js')}}"></script>
+    <script src="{{ asset('exercises/exercises.js') }}"></script>
 
     <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            fetchWorkouts({{ $appointmentID }})
+        });
         //used on 2 places
         function fetchWorkouts(appointmentId) {
-            fetch('{{route('appointment.workouts')}}', {
-                method: 'POST',
-                body: JSON.stringify({ appointmentId: appointmentId }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                }
-            })
+            fetch('{{ route('appointment.workouts') }}', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        appointmentId: appointmentId
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                    }
+                })
                 .then(response => response.text())
                 .then(html => {
                     // Update the DOM with the fetched Blade view
@@ -80,8 +83,5 @@
                 })
                 .catch(error => console.error('Error fetching workout exercises: ' + error));
         }
-        document.addEventListener("DOMContentLoaded", function () {
-            fetchWorkouts({{$appointmentID}})
-        });
     </script>
 @endsection
